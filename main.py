60a66e76a2d6023d2d8940d3ed329570575af064
mouhamadhaydar <mhamad.haydar@hotmail.com>
2026-03-26 21:23:10 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
 @app.route("/")
 def home():
-    return "API is running"
+    return "Render app is running"
+
+@app.route("/health")
+def health():
+    return {"status": "ok"}
+
+@app.route("/ocr", methods=["POST"])
+def ocr():
+    return jsonify({"message": "OCR endpoint ready"})
